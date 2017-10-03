ee219b114561d4a9d5efe6a2ecec291164db6fd5
Sean Omlor <somlor@eml.cc>
2017-10-03 00:45:20 -0400

    axe overrideDerivation

--- configuration.nix
+++ configuration.nix
@@ -20,13 +20,7 @@
 
   environment = {
     systemPackages = with pkgs; [
-      (pkgs.lib.overrideDerivation pkgs.beam.packages.erlangR20.elixir (attrs: {
-        LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-        LANG = "en_US.UTF-8";
-        LC_COLLATE = "en_US.UTF-8";
-        LC_CTYPE = "en_US.UTF-8";
-        LC_ALL = "en_US.UTF-8";
-      }))
+      beam.packages.erlangR20.elixir
       curl
       gcc
       git
