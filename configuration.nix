4cbd6720fb8814f20ffce5cd0c661a39d7a5c253
Sean Omlor <somlor@eml.cc>
2017-10-19 21:43:42 -0400

    simple

--- configuration.nix
+++ configuration.nix
@@ -20,13 +20,6 @@
 
   environment = {
     systemPackages = with pkgs; [
-      (pkgs.lib.overrideDerivation pkgs.beam.packages.erlangR20.elixir (attrs: {
-        LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-        LANG = "en_US.UTF-8";
-        LC_COLLATE = "en_US.UTF-8";
-        LC_CTYPE = "en_US.UTF-8";
-        LC_ALL = "en_US.UTF-8";
-      }))
       curl
       gcc
       git
